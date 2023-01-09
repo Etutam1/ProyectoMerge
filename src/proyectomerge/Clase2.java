3089994d7202fa9f3d49bfe39adb49a6e28c1859
a22lucasmpg <a22lucasmpg@W10D-I8E30.mshome.net>
2023-01-09 12:10:09 +0100

    Inicial

---
+++ src/proyectomerge/Clase2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectomerge;
+
+/**
+ *
+ * @author a22lucasmpg
+ */
+public class Clase2 {
+    
+}
